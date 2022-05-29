d22903b9b0d78ea1d93a11b2e33a13cc1408bf4d
Pavel Sarlov <sarlovpavel@gmail.com>
2022-05-29 19:27:38 +0300

    fix keys and user optional fields

--- db.sql
+++ db.sql
@@ -4,18 +4,18 @@ USE ccs_db;
 
 CREATE TABLE
   IF NOT EXISTS users(
-    id VARCHAR(16) PRIMARY KEY DEFAULT UUID(),
+    id CHAR(36) PRIMARY KEY DEFAULT UUID(),
     name VARCHAR(50) NOT NULL,
     password VARCHAR(60) NOT NULL,
-    year INT NOT NULL,
-    speciality VARCHAR(50) NOT NULL,
-    faculty VARCHAR(50) NOT NULL,
+    year INT,
+    speciality VARCHAR(50),
+    faculty VARCHAR(50),
     role VARCHAR(50) NOT NULL
   );
 
 CREATE TABLE
   IF NOT EXISTS chat_rooms(
-    id VARCHAR(16) PRIMARY KEY DEFAULT UUID(),
+    id CHAR(36) PRIMARY KEY DEFAULT UUID(),
     name VARCHAR(50) NOT NULL,
     availability_date DATETIME DEFAULT NULL,
     is_active BOOLEAN DEFAULT TRUE NOT NULL
@@ -23,11 +23,10 @@ CREATE TABLE
 
 CREATE TABLE
   IF NOT EXISTS user_chats(
-    id VARCHAR(16) PRIMARY KEY DEFAULT UUID(),
-    user_id VARCHAR(16) NOT NULL,
-    chat_id VARCHAR(16) NOT NULL,
+    id CHAR(36) PRIMARY KEY DEFAULT UUID(),
+    user_id CHAR(36) NOT NULL,
+    chat_id CHAR(36) NOT NULL,
     is_anonymous BOOLEAN DEFAULT TRUE NOT NULL,
-
     CONSTRAINT fk_user_chats__users FOREIGN KEY(user_id) REFERENCES users(id) ON DELETE CASCADE,
     CONSTRAINT fk_user_chats__chat_rooms FOREIGN KEY(chat_id) REFERENCES chat_rooms(id) ON DELETE CASCADE,
     UNIQUE(user_id, chat_id)
@@ -35,13 +34,13 @@ CREATE TABLE
 
 CREATE TABLE
   IF NOT EXISTS messages(
-    id VARCHAR(16) PRIMARY KEY DEFAULT UUID(),
-    user_id VARCHAR(16) NOT NULL,
-    chat_id VARCHAR(16) NOT NULL,
+    id CHAR(36) PRIMARY KEY DEFAULT UUID(),
+    user_id CHAR(36) NOT NULL,
+    chat_id CHAR(36) NOT NULL,
     content VARCHAR(2000),
     date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     is_disabled BOOLEAN DEFAULT TRUE NOT NULL,
-
     CONSTRAINT fk_messages__users FOREIGN KEY(user_id) REFERENCES users(id) ON DELETE CASCADE,
     CONSTRAINT fk_messages__chat_rooms FOREIGN KEY(chat_id) REFERENCES chat_rooms(id) ON DELETE CASCADE
   );
+
