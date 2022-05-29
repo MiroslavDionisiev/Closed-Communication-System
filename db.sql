25a2681b4ac74540e3dcf8dd5327a2208de78c3b
Pavel Sarlov <sarlovpavel@gmail.com>
2022-05-29 15:59:33 +0300

    fixed naming convention

--- db.sql
+++ db.sql
@@ -2,7 +2,8 @@ CREATE DATABASE IF NOT EXISTS ccs_db;
 
 USE ccs_db;
 
-CREATE TABLE IF NOT EXISTS users(
+CREATE TABLE
+  IF NOT EXISTS users(
     id VARCHAR(16) PRIMARY KEY DEFAULT UUID(),
     name VARCHAR(50) NOT NULL,
     password VARCHAR(60) NOT NULL,
@@ -10,36 +11,37 @@ CREATE TABLE IF NOT EXISTS users(
     speciality VARCHAR(50) NOT NULL,
     faculty VARCHAR(50) NOT NULL,
     role VARCHAR(50) NOT NULL
-);
+  );
 
-CREATE TABLE IF NOT EXISTS chat_rooms(
+CREATE TABLE
+  IF NOT EXISTS chat_rooms(
     id VARCHAR(16) PRIMARY KEY DEFAULT UUID(),
     name VARCHAR(50) NOT NULL,
-    availabilityDate DATETIME DEFAULT NULL,
-    isActive BOOLEAN DEFAULT TRUE NOT NULL
-);
+    availability_date DATETIME DEFAULT NULL,
+    is_active BOOLEAN DEFAULT TRUE NOT NULL
+  );
 
-CREATE TABLE IF NOT EXISTS user_chats(
+CREATE TABLE
+  IF NOT EXISTS user_chats(
     id VARCHAR(16) PRIMARY KEY DEFAULT UUID(),
-    userId VARCHAR(16) NOT NULL,
-    chatId VARCHAR(16) NOT NULL,
-    isAnonymous BOOLEAN DEFAULT TRUE NOT NULL,
+    user_id VARCHAR(16) NOT NULL,
+    chat_id VARCHAR(16) NOT NULL,
+    is_anonymous BOOLEAN DEFAULT TRUE NOT NULL,
 
-    CONSTRAINT fk_user_chats__users FOREIGN KEY(userId) REFERENCES users(id) ON DELETE CASCADE,
-    CONSTRAINT fk_user_chats__chat_rooms FOREIGN KEY(chatId) REFERENCES chat_rooms(id) ON DELETE CASCADE,
-    UNIQUE(userId, chatId)
-);
+    CONSTRAINT fk_user_chats__users FOREIGN KEY(user_id) REFERENCES users(id) ON DELETE CASCADE,
+    CONSTRAINT fk_user_chats__chat_rooms FOREIGN KEY(chat_id) REFERENCES chat_rooms(id) ON DELETE CASCADE,
+    UNIQUE(user_id, chat_id)
+  );
 
-CREATE TABLE IF NOT EXISTS messages(
+CREATE TABLE
+  IF NOT EXISTS messages(
     id VARCHAR(16) PRIMARY KEY DEFAULT UUID(),
-    userId VARCHAR(16) NOT NULL,
-    chatId VARCHAR(16) NOT NULL,
+    user_id VARCHAR(16) NOT NULL,
+    chat_id VARCHAR(16) NOT NULL,
     content VARCHAR(2000),
     date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    isDisabled BOOLEAN DEFAULT TRUE NOT NULL,
+    is_disabled BOOLEAN DEFAULT TRUE NOT NULL,
 
-    CONSTRAINT fk_messages__users FOREIGN KEY(userId) REFERENCES users(id) ON DELETE CASCADE,
-    CONSTRAINT fk_messages__chat_rooms FOREIGN KEY(chatId) REFERENCES chat_rooms(id) ON DELETE CASCADE,
-    UNIQUE(userId, chatId)
-);
-
+    CONSTRAINT fk_messages__users FOREIGN KEY(user_id) REFERENCES users(id) ON DELETE CASCADE,
+    CONSTRAINT fk_messages__chat_rooms FOREIGN KEY(chat_id) REFERENCES chat_rooms(id) ON DELETE CASCADE
+  );
